014b6a29a14bf96de3aa76d3200f16cedc5ba444
Param Pal Singh <singhparampal97@gmail.com>
2023-01-16 14:57:19 +0100

    test fix for assets pt 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["/multi-step-form/assets/images/**"],
+  assetsInclude: ["assets/images/*.svg"],
   base: "/multi-step-form/",
 });
