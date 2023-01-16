ba95f77c0b10368c27780ee5ef96e8406be12ad8
Param Pal Singh <singhparampal97@gmail.com>
2023-01-16 23:51:53 +0100

    layout for summary page && add hover border for options

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,7 +1,7 @@
 const Option = ({ onSelect, selected, children }) => {
   return (
     <div
-      className={`flex py-5 gap-2 justify-start rounded-xl border ${
+      className={`hover:border-indigo-600 flex py-5 gap-2 justify-start rounded-xl border ${
         selected && " border-indigo-800"
       }`}
       onClick={onSelect}
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,11 +1,31 @@
 const Summary = () => {
   return (
-    <div>
-      Finishing up Double-check everything looks OK before confirming.
-      {
-        //TODO: Dynamically add subscription and add-on selections here
-      }
-      Total (per month/year) Go Back Confirm
+    <div className="h-full w-11/12 mx-auto md:px-20 py-16">
+      <h1 className="info__head mt-10 font-extrabold text-2xl md:text-4xl">
+        Finishing up
+      </h1>
+      <p className="info__p">
+        Double-check everything looks OK before confirming.
+      </p>
+      <div className="rounded-md mt-10 bg-gray-50 px-5">
+        <div className="flex justify-between py-5">
+          <h2 className="info__head font-medium">Arcade (Monthly)</h2>
+          <span className="info__head font-extrabold">$ 9/mo</span>
+        </div>
+        <hr />
+        <div className="flex justify-between py-1.5">
+          <p className="info__p">Online service</p>
+          <span className="info__head ">+$1/mo</span>
+        </div>
+        <div className="flex justify-between py-1.5">
+          <p className="info__p">Larger storage</p>
+          <span className="info__head ">+$2/mo</span>
+        </div>
+      </div>
+      <div className="flex justify-between px-5 mt-8">
+        <p className="info__p ">Total(per month)</p>
+        <span className="text-indigo-600 font-extrabold text-lg">+$12/mo</span>
+      </div>
     </div>
   );
 };
