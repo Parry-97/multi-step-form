bbf5a72f1ea874d76d96a5c326ae1f4d3e4119dc
Parampal Singh <singhparampal97@gmail.com>
2023-01-16 18:26:32 +0100

    Update vite config.js for assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["./images/**"],
+  assetsInclude: ["/multi-step-form/images/**"],
   base: "/multi-step-form/",
 });
